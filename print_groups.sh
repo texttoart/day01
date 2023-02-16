0312054f14dec9bee813fdb05a9b6724f756ba13
texttoart <123409532+texttoart@users.noreply.github.com>
2023-02-16 13:56:04 +0100

    Delete print_groups.sh

--- print_groups.sh
+++
@@ -1,1 +0,0 @@
-uid=502(shankargamas),gid=20(staff),groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),101(com.apple.access_screensharing-disabled),399(com.apple.access_ssh),400(com.apple.access_remote_ae)
